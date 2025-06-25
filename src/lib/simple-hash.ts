75e4e9c524d8c9f63e4b4d3655b90bda4af67e56
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-06-25 12:14:40 +0200

    cleanup

--- src/lib/simple-hash.ts
+++
@@ -1,9 +0,0 @@
-const simpleHash = (str: string) => {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = ((hash << 5) - hash + str.charCodeAt(i)) | 0;
-  }
-  return (hash >>> 0).toString(36);
-};
-
-export default simpleHash;
